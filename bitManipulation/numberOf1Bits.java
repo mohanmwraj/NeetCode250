c8612fd9aeca570fb79bc4f5ec5e10c8f1621d82
Mohan Raj <mohanmwraj@gmail.com>
2025-02-03 18:57:09 +0530

    Bit Manipulation

--- bitManipulation/numberOf1Bits.java
+++ bitManipulation/numberOf1Bits.java
@@ -1,4 +1,63 @@
 package bitManipulation;
 
 public class numberOf1Bits {
+    /*
+        * Approach: Bit Mask - I
+        *
+     */
+    public int hammingWeight_1(int n){
+        int res = 0;
+        for(int i = 0; i < 32; ++i){
+            if((1 << i & n) != 0){
+                res++;
+            }
+        }
+
+        return res;
+    }
+    /*
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+     */
+    /*
+        * Approach: Bit Mask II
+        *
+     */
+    public int hammingWeight_2(int n){
+        int res = 0;
+        while(n != 0){
+            res += (n & 1) == 1 ? 1 : 0;
+            n >>= 1;
+        }
+        return res;
+    }
+
+    /*
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+     */
+    /*
+        * Approach: Bit Mask Optimal
+        *
+     */
+    public int hammingWeight_3(int n){
+        int res = 0;
+        while(n != 0){
+            n &= n - 1;
+            res++;
+        }
+
+        return res;
+    }
+    /*
+        Time Complexity: O(1)
+        Space Complexity: O(1)
+     */
+    /*
+        * Approach: Built-In function
+        *
+     */
+    public int hammingWeight_4(int n){
+        return Integer.bitCount(n);
+    }
 }
--- bitManipulation/singleNumber.java
+++ bitManipulation/singleNumber.java
@@ -1,4 +1,87 @@
 package bitManipulation;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class singleNumber {
+    /*
+        * Approach: Brute Force
+        *
+     */
+    public int singleNumber_1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            boolean flag = true;
+            for (int j = 0; j < nums.length; j++) {
+                if (i != j && nums[i] == nums[j]) {
+                    flag = false;
+                    break;
+                }
+            }
+            if (flag) {
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+    /*
+        Time Complexity: O(n ^ 2)
+        Space Complexity: O(1)
+     */
+
+    /*
+        * Approach: Hash Set
+        *
+     */
+    public int singleNumber_2(int[] nums) {
+        Set<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (seen.contains(num)) {
+                seen.remove(num);
+            } else {
+                seen.add(num);
+            }
+        }
+        return seen.iterator().next();
+    }
+    /*
+        Time Complexity: O(n)
+        Space Complexity: O(n)
+     */
+
+    /*
+     * Approach: sorting
+     *
+     */
+    public int singleNumber_3(int[] nums) {
+        Arrays.sort(nums);
+        int i = 0;
+        while (i < nums.length - 1) {
+            if (nums[i] == nums[i + 1]) {
+                i += 2;
+            } else {
+                return nums[i];
+            }
+        }
+        return nums[i];
+    }
+    /*
+        Time Complexity: O(n log n)
+        Space Complexity: O(n)
+     */
+    /*
+        * Approach: Bit Manipulation
+        *
+     */
+    public int singleNumber_4(int[] nums) {
+        int res = 0;
+        for (int num : nums) {
+            res ^= num;
+        }
+        return res;
+    }
+    /*
+        Time Complexity: O(n)
+        Space Complexity: O(1)
+     */
 }
