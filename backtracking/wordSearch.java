2ffb7d5a52204763b9cf31351abfc6e98176f0ab
Mohan Raj <mohanmwraj@gmail.com>
2025-02-04 18:31:38 +0530

    Backtracking

--- backtracking/wordSearch.java
+++ backtracking/wordSearch.java
@@ -1,9 +1,58 @@
 package backtracking;
 
-import trees.Pair;
 
 import java.util.HashSet;
 import java.util.Set;
+
+class Pair<First, Second> {
+    private First first;
+    private Second second;
+
+    public Pair(First first, Second second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public void setFirst(First first) {
+        this.first = first;
+    }
+
+    public void setSecond(Second second) {
+        this.second = second;
+    }
+
+    public First getKey() {
+        return first;
+    }
+
+    public Second getValue() {
+        return second;
+    }
+
+    public void set(First first, Second second) {
+        setFirst(first);
+        setSecond(second);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Pair pair = (Pair) o;
+        if (first != null ? !first.equals(pair.first) : pair.first != null) return false;
+        if (second != null ? !second.equals(pair.second) : pair.second != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first != null ? first.hashCode() : 0;
+        result = 31 * result + (second != null ? second.hashCode() : 0);
+        return result;
+    }
+}
 
 public class wordSearch {
     /*
@@ -11,7 +60,7 @@ public class wordSearch {
         *
      */
     private int ROWS, COLS;
-    private Set<Pair<Integer, Integer>> path = new HashSet<>();
+    Set<Pair<Integer, Integer>> path = new HashSet<>();
 
     public boolean exist(char[][] board, String word) {
         ROWS = board.length;
